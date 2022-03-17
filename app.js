06bc1a9a2775475a6d28fb7894a5d3c4160dd957
Hamza Bukhari <hambukhari@gmail.com>
2022-03-17 20:49:30 +0500

    added files

---
+++ app.js
@@ -0,0 +1,77 @@
+// var array = ["karachi","lahore", "peshawar","abc", "quetta","islamabad"];
+// array[2] = "islamabad";
+// array.push("Islamabad"); ADDING IN END
+// array.pop();  REMOVING FROM END
+// array.shift(); REMOVING FROM START
+// array.unshift("Quetta"); ADDING IN START
+// array.splice(3,2,"abc"); //konse index pr add krna, kitne remove krne hain, kia add krna hai
+// var notaCity = array.slice(3);
+// console.log(notaCity);
+
+// for (var i = 0; i <= array.length; i++) {
+//     array[i] = array[i].toUpperCase();
+//     console.log(array[i]);
+//     }
+
+
+// var text = "hamza";
+// var letter = text.charAt(3);
+// var text = ["hamza","arzoo","dania"];
+// var letter = text.indexOf("dania");
+// console.log(text);
+// console.log(text[1]);
+// console.log(letter);
+
+
+// var num = 2.6;
+// var num1 = 2.4;
+// var num2 = 2.5;
+// console.log(num,num1,num2);
+// console.log(Math.round(num));
+// console.log(Math.round(num1));
+// console.log(Math.round(num2));
+// var a = Math.random();
+// var Num = (a * 6) + 1;
+// console.log(Math.floor(Num));
+// console.log(Math.ceil(num));
+// console.log(Math.floor(num));
+
+// var num = 2.12321313123123;
+// console.log(num.toFixed(2));
+
+//  var now = new Date();
+//  var theHr = now.getHours();
+//  var theMin = now.getMinutes();
+//  alert("time: " + theHr + ":" + theMin);
+
+// function currenttime(){
+//     var now = new Date();
+//     var theHr = now.getHours();
+//     var theMin = now.getMinutes();
+//  alert("time: " + theHr + ":" + theMin);
+// }
+// currenttime();
+
+// function tax() {
+//     var a = 100;
+//     return a;
+// }
+// function buy() {
+//     var name = document.getElementById("name").value;
+//     var n = parseInt(name);
+//     var sum = n + tax();
+
+//     console.log(name);
+//     console.log(sum);
+// }
+
+// var plan1 = {
+//     name: "basic",
+//     price: 3.99,
+//     space: 100,
+//     transfer: 1000,
+//     pages: 10
+    
+// }
+// plan1.discount = 10;
+// console.log(plan1)
